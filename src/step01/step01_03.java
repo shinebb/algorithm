39da76350cca1b483738ee5611b8b788b68b9909
Dohee Kim <ododalrio@gmail.com>
2024-08-06 22:25:01 +0900

    240806 update

---
+++ src/step01/step01_03.java
@@ -0,0 +1,16 @@
+package step01;
+
+import java.util.Scanner;
+
+/*
+* 두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+* */
+public class step01_03 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a - b);
+    }
+}
