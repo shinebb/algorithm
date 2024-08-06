2ed993f0a4a9dffebed4987467ac5baea1d4fed0
Dohee Kim <ododalrio@gmail.com>
2024-08-06 22:26:35 +0900

    240806 update

--- src/step01/step01_05.java
+++ src/step01/step01_05.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 /*
 * 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+* 조건 : 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
 * */
 public class step01_05 {
     public static void main(String[] args){
