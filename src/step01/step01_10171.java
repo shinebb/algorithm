1867e73af9b2d93c303307fbd278e9806c028d87
dohee <ododalrio@gmail.com>
2024-08-08 17:45:35 +0900

    240808 update

---
+++ src/step01/step01_10171.java
@@ -0,0 +1,15 @@
+package step01;
+
+import java.util.Scanner;
+
+/*
+* 고양이를 출력하시오.
+ * */
+public class step01_10171 {
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ src/step01/step01_10172.java
@@ -0,0 +1,14 @@
+package step01;
+
+/*
+* 고양이를 출력하시오.
+ * */
+public class step01_10172 {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
