8083b9ffcb73b4f1820c6c944999d03ab52ef83d
dohee <ododalrio@gmail.com>
2024-08-08 17:49:15 +0900

    240808 update

--- src/step01/step01_10869.java
+++ src/step01/step01_10869.java
@@ -3,13 +3,18 @@ package step01;
 import java.util.Scanner;
 
 /*
-* 불기 연도를 서기 연도로 변환한 결과를 출력한다.
+* 두 정수 A와 B를 입력받은 다음,  A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
  * */
 public class step01_10869 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
+        int b = in.nextInt();
 
-        System.out.println(a-543);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
     }
 }
---
+++ src/step01/step01_18108.java
@@ -0,0 +1,16 @@
+package step01;
+
+import java.util.Scanner;
+
+/*
+ * 불기 연도를 서기 연도로 변환한 결과를 출력한다.
+ * */
+public class step01_18108 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        System.out.println(a-543);
+    }
+}
+
